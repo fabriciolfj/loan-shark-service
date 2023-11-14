d70cf199b75eebbafc9bebb0b8d956287a04622c
fabricio jacob <fabrluc@outlook.com>
2023-11-14 12:06:51 -0300

    add

---
+++ src/main/java/com/gifthub/loanshark/configuration/ConfigBeans.java
@@ -0,0 +1,26 @@
+package com.gifthub.loanshark.configuration;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+public class ConfigBeans {
+
+    @Bean
+    public MessageSource messageSource() {
+        final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    @Primary
+    public LocalValidatorFactoryBean getValidator() {
+        final LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
