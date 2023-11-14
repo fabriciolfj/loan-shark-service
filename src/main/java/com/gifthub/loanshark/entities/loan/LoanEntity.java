9dc4b12de1b2a63c432901e818f1751b39f26006
fabricio jacob <fabrluc@outlook.com>
2023-11-14 11:26:49 -0300

    add

--- src/main/java/com/gifthub/loanshark/entities/loan/LoanEntity.java
+++ src/main/java/com/gifthub/loanshark/entities/loan/LoanEntity.java
@@ -4,9 +4,22 @@ package com.gifthub.loanshark.entities.loan;
 import com.gifthub.loanshark.entities.loan.vo.CustomerEntity;
 import com.gifthub.loanshark.entities.loan.vo.DetailsLoanEntity;
 import com.gifthub.loanshark.entities.loan.vo.StatusLoanEntity;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class LoanEntity {
 
+    @EqualsAndHashCode.Include
+    private String code;
     private CustomerEntity customerEntity;
     private DetailsLoanEntity details;
     private StatusLoanEntity status;
@@ -15,6 +28,11 @@ public class LoanEntity {
         this.customerEntity = customer;
         this.details = details;
         this.status = StatusLoanEntity.PENDING;
+        this.code = UUID.randomUUID().toString();
+    }
+
+    public BigDecimal getSalary() {
+        return customerEntity.salary();
     }
 
 }
--- src/main/java/com/gifthub/loanshark/entities/loan/vo/CustomerEntity.java
+++ src/main/java/com/gifthub/loanshark/entities/loan/vo/CustomerEntity.java
@@ -1,6 +1,7 @@
 package com.gifthub.loanshark.entities.loan.vo;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record CustomerEntity(String name, String document, LocalDate birthday) {
+public record CustomerEntity(String name, String document, LocalDate birthday, BigDecimal salary) {
 }
